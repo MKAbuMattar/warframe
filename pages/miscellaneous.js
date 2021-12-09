9a0afa48bc4896af358150c6f40cde89c621672a
mkabumattar <mohammad.khaled@outlook.com>
2021-12-10 00:38:06 +0200

    hotfix icons + add miscellaneous

---
+++ pages/miscellaneous.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react'
+
+import Miscellaneous from '../components/Miscellaneous/Miscellaneous'
+
+const index = () => {
+  return (
+    <Fragment>
+      <Miscellaneous />
+    </Fragment>
+  )
+}
+
+export default index
