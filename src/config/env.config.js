c7e6ecde0afe73ddf8e22ca225ea82357e4a3af9
MKAbuMattar <mohammad.khaled@outlook.com>
2024-09-02 23:50:43 +0300

    refactor: Export NODE_ENV and HOST as constants in env.config.js

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -2,10 +2,5 @@ const {NEXT_PUBLIC_NODE_ENV} = process.env;
 
 const {NEXT_PUBLIC_HOST} = process.env;
 
-const NODE_ENV = NEXT_PUBLIC_NODE_ENV;
-const HOST = NEXT_PUBLIC_HOST;
-
-export default {
-  NODE_ENV,
-  HOST,
-};
+export const NODE_ENV = NEXT_PUBLIC_NODE_ENV;
+export const HOST = NEXT_PUBLIC_HOST;
